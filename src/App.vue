ecb7c7a39170dd6982d693e37f0d9eb917e68077
Chu I-Ting <itingchu1005@gmail.com>
2024-11-05 23:21:04 +0800

    add BarChartRace embedded

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BarChartRace />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BarChartRace from './components/BarChartRace.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BarChartRace
   }
 }
 </script>
